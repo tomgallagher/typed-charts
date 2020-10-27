2668a2c527f666fa077c90c47fd06bb8d3828c50
Tom Gallagher <thomasgallagher101@gmail.com>
2020-10-27 13:52:20 +0000

    added animated tree chart

--- src/components/animatedTreeExample.tsx
+++ src/components/animatedTreeExample.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
-import { Header, Button } from 'semantic-ui-react';
+import { Header } from 'semantic-ui-react';
 import { useDomToJson } from './../hooks/useDomToJson';
 import { AnimatedTreeChart } from './../chartComponents/animatedTreeChart';
 
 export const AnimatedTreeExample = () => {
-    const jsonData = useDomToJson(document.getElementById('root'));
-
+    const [jsonData] = useDomToJson(document.getElementById('root'));
     return (
         <div className='chartWrapper'>
             <Header textAlign='center'>Animated Dom Tree Chart</Header>
             <AnimatedTreeChart data={jsonData} />
-            <div className='chartControls'>
-                <Button>Update</Button>
-            </div>
         </div>
     );
 };
--- src/hooks/useDomToJson.ts
+++ src/hooks/useDomToJson.ts
@@ -9,10 +9,14 @@ type DomJson = {
 };
 
 export const useDomToJson = (node: HTMLElement | null) => {
-    const [json, setJson] = useState<DomJson | null>(null);
+    //use DomToJson function to set the initial state
+    const [json, setJson] = useState<DomJson | null>(DomToJson(node));
+    //then add useEffect hook so it can update when the node appears in the dom
     useEffect(() => {
-        const json = DomToJson(node);
-        setJson(json);
+        setJson(DomToJson(node));
     }, [node]);
-    return json;
+    //allow for the data to be updated
+    const updateJson = (json: DomJson | null) => setJson(json);
+    // here, we freeze the array to a tuple
+    return [json, updateJson] as const;
 };
