e2173c103b868e08e095b2b2707b25a2959299b6
Tom Gallagher <thomasgallagher101@gmail.com>
2020-10-30 09:57:05 +0000

    made world map zoomable"

--- src/chartComponents/worldMap.js
+++ src/chartComponents/worldMap.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { geoMercator, geoPath, select } from 'd3';
 import { useResizeObserver } from '../hooks/useResizeObserver';
 
@@ -6,6 +6,7 @@ import { useResizeObserver } from '../hooks/useResizeObserver';
 //https://github.com/muratkemaldar/using-react-hooks-with-d3/
 
 export const WorldMap = ({ data, property }) => {
+    const [selectedCountry, setSelectedCountry] = useState(null);
     const svgRef = useRef();
     const wrapperRef = useRef();
     const dimensions = useResizeObserver(wrapperRef);
@@ -19,28 +20,54 @@ export const WorldMap = ({ data, property }) => {
         //we define the projection function - translates lat/long data from geojson into 2 dimensional pixel values
         const projection = geoMercator()
             //and make sure the projection output is sensitive to the size and what data it is going to parse
-            .fitSize([width, height], data);
+            .fitSize([width, height], selectedCountry || data)
+            //add precision for smooth zoom scrolling
+            .precision(100);
         //then we need a function to generate the paths for the outlines of each of the countries
         const pathGenerator = geoPath().projection(projection);
+        //render the country
         svg.selectAll('.country')
             //in geo.json data all the countries are defined in the features array
             .data(data.features)
             //transmute the data into svg elements
             .join('path')
+            //then a click handler to handle zoom
+            .on('click', function (event, feature) {
+                const index = svg.selectAll('.country').nodes().indexOf(this);
+                console.log(index);
+                setSelectedCountry(selectedCountry === feature ? null : feature);
+            })
             //add the class so it can be changed
             .attr('class', 'country')
+            //allow zoom transition
+            .transition()
+            .duration(1000)
             //add the shape of each path element
             .attr('d', (feature) => pathGenerator(feature))
             //make it look nice
-            .style('stroke', '#888')
-            .style('stroke-width', '1px')
-            .style('fill', '#e5e5e5')
-            .style('opacity', '.6');
-    }, [data, dimensions, property]);
+            .attr('stroke', '#888')
+            .attr('stroke-width', '1px')
+            .attr('fill', '#e5e5e5')
+            .attr('opacity', '.6');
+        //render the label of the country on zoom
+        svg.selectAll('.label')
+            //use the selected country as the only data point
+            .data([selectedCountry])
+            //create text
+            .join('text')
+            .attr('class', 'label')
+            .text(
+                //add the details from the feature
+                (feature) => feature && feature.properties.name + ': ' + feature.properties[property].toLocaleString()
+            )
+            //position in top left
+            .attr('x', 10)
+            .attr('y', 25);
+    }, [data, dimensions, property, selectedCountry]);
 
     return (
         <div className='chartContainer' ref={wrapperRef}>
-            <svg ref={svgRef} style={{ width: '100%', height: '300px', overflow: 'visible' }} />
+            <svg ref={svgRef} style={{ width: '100%', height: '300px', overflow: 'hidden' }} />
         </div>
     );
 };
